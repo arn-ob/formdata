881439883f8f629db9a65eeffad101d2fe3e4bca
Tanzim Saqib <tanzimsaqib@live.com>
2015-06-03 11:43:45 +0600

    Trying to deploy to Azure

---
+++ app.js
@@ -0,0 +1,29 @@
+/// <reference path="typings/node/node.d.ts"/>
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./framework/routes')(app);
+
+module.exports = app;
+
+
---
+++ framework/routes.js
@@ -0,0 +1,40 @@
+module.exports = function(app)
+{
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Hello World.' });
+  });  
+  
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+    next(err);
+  });
+  
+  // error handlers
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+  
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+};
